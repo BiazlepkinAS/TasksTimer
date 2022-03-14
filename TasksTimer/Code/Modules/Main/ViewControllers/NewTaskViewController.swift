9d22a1184daabd9baeb9569bc8694ccd14d9c310
Andrei Bezlepkin <6abez6@gmail.com>
2022-03-14 18:19:18 +0300

    add viewModel and Storyboard config

---
+++ TasksTimer/Code/Modules/Main/ViewControllers/NewTaskViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewTaskViewController.swift
+//  TasksTimer
+//
+//  Created by Andrei Bezlepkin on 14.03.22.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+}
