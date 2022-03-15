26b5ba297ef24b26829891a1fbc9420437ebba1a
Andrei Bezlepkin <6abez6@gmail.com>
2022-03-15 10:59:11 +0300

    add collectionView and cell

---
+++ TasksTimer/Code/Modules/Main/Cells/TaskTypeCell.swift
@@ -0,0 +1,65 @@
+//
+//  TaskTypeCell.swift
+//  TasksTimer
+//
+//  Created by Andrei Bezlepkin on 15.03.22.
+//
+
+import UIKit
+
+class TaskTypeCell: UICollectionViewCell {
+    
+    @IBOutlet var imageContainer: UIView!
+    @IBOutlet var imageTypeView: UIImageView!
+    @IBOutlet var typeNameLabel: UILabel!
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        
+        DispatchQueue.main.async {
+            self.imageContainer.layer.cornerRadius = self.imageContainer.bounds.width / 2
+        }
+        
+    }
+    
+    
+    override class func description() -> String {
+        return "TaskTypeCell"
+    }
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        resetCell()
+        self.imageTypeView.image = nil
+        
+    }
+    
+    
+    private func setupCell(taskType: TaskType, isSelected: Bool) {
+        self.typeNameLabel.text = taskType.typeName
+        
+        if (isSelected) {
+            self.imageContainer.backgroundColor = UIColor(hex: "17b890").withAlphaComponent(0.5)
+            self.typeNameLabel.textColor = UIColor(hex: "006666")
+            self.imageTypeView.tintColor = UIColor.white
+            self.imageTypeView.image = UIImage(systemName: taskType.symbolName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 26, weight: .medium))
+        }else  {
+            self.imageTypeView.image = UIImage(systemName: taskType.symbolName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 24, weight: .regular))
+            resetCell()
+        }
+    }
+    
+    private func resetCell() {
+        self.imageContainer.backgroundColor = UIColor.clear
+        self.typeNameLabel.textColor = UIColor.black
+        self.imageTypeView.tintColor = UIColor.black
+    }
+    
+    
+
+}
--- TasksTimer/Code/Modules/Main/ViewControllers/NewTaskViewController.swift
+++ TasksTimer/Code/Modules/Main/ViewControllers/NewTaskViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class NewTaskViewController: UIViewController {
     
+    @IBOutlet var closedButton: UIButton!
+    @IBOutlet var taskNameTF: UITextField!
+    @IBOutlet var taskDescription: UITextField!
+    @IBOutlet var taskContainer: UIView!
+    @IBOutlet var hoursTF: UITextField!
+    @IBOutlet var minutiesTF: UITextField!
+    @IBOutlet var secondsTF: UITextField!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    @IBAction func closedButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
         
     }
     
